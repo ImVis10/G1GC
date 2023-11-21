d7fd5b40c33eb22aaf8de40a68576a76fba08ff6
ImVis10 <viswanathvankadara@gmail.com>
2023-11-21 14:50:25 -0500

    chore: remove commented code

--- HeapTest.java
+++ HeapTest.java
@@ -5,18 +5,14 @@ import java.util.Iterator;
 public class HeapTest {
 
     public static void main(String[] args) {
-        // Create a map with a Boolean key and Integer value
         Map<Boolean, Integer> map = new HashMap<Boolean, Integer>();
 
-        // Fill the map with objects
         for (int i = 0; i < 1000; i++) {
             map.put(i % 2 == 0, i);
         }
 
-        // Print memory usage before deletion
-        // printMemoryUsage("Before deletion");
 
-        // Delete objects based on a condition (e.g., delete if key is true)
+        // Delete if key is true
         Iterator<Map.Entry<Boolean, Integer>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<Boolean, Integer> entry = iterator.next();
@@ -24,12 +20,6 @@ public class HeapTest {
                 iterator.remove();
             }
         }
-
-        // Suggest garbage collection
-        // System.gc();
-
-        // Print memory usage after deletion
-        // printMemoryUsage("After deletion");
     }
 
     private static void printMemoryUsage(String message) {
